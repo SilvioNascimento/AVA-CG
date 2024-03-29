aef980b31f561defe4b432e7c0b107085afc10ad
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-03-29 19:27:06 -0300

    Alterando de '/cadastro' para '/Register'

--- AVA-CG/src/main.jsx
+++ AVA-CG/src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path: "/cadastro",
+        path: "/Register",
         element: <Register />
       },
       {
--- AVA-CG/src/routes/Register/Register.jsx
+++ AVA-CG/src/routes/Register/Register.jsx
@@ -1,55 +1,47 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Register = () => {
-
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
-    const handleRegisterClick = (e) => {
-        e.preventDefault();
-      
-        // Crie um objeto com os dados do usuário
+    const handleRegisterClick = async () => {
         const userData = {
-          nome: nome,
-          email: email,
-          senha: senha
+          'nome': nome,
+          'email': email,
+          'senha': senha
         };
 
+        try {
+            const response = await axios.post('/Register', userData);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao cadastrar o usuário:', error.message);
+        }
     }
-    
 
     return (
         <div className="Area-Register-e-Login">
-            
             <div className="Area-Register">
-
                 <div className="Register">CRIE SUA CONTA</div>
-
                 <form className="register-Form">
                     <input type="text" id="nome" className="register-Input" placeholder="Nome" onChange={(e) => setNome(e.target.value)} />
                     <input type="text" id="email" className="register-Input" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
                     <input type="password" id="senha" className="register-Input" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
-
                     <div className="Area-btn-Register">
                         <div className="register-btn">
-                            <Link to='/'>
-                                <button type="submit" onChange={(e) => handleRegisterClick(e)}>Cadastrar</button>
-                            </Link>
+                            <button type="button" onClick={handleRegisterClick}>Cadastrar</button>
                         </div>
-
                         <div className="back-btn">
                             <Link to='/'>
                                 <button>Voltar para Login</button>
                             </Link>
                         </div>
                     </div>
-
                 </form>
-
             </div>
-
         </div>
     );
 }
