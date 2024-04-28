0e3653a58e3d6e57c61b8f5b9a8b1ea8694cf3ff
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-04-28 17:45:45 -0300

    Retirando o navbar e footer

--- AVA-CG/src/App.jsx
+++ AVA-CG/src/App.jsx
@@ -7,9 +7,12 @@ function App() {
 //  const [count, setCount] = useState(0)
 
   return (
+    <div className='container'>
       <div className='App'>
         <Outlet />
       </div>
+    </div>
+      
 
   )
 }
