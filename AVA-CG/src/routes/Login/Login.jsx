dba66acb5d3fa1e371d6d97aca04c469017b520d
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-04-28 17:49:31 -0300

    Adicionando a funcionalidade de verificar se existe tal usuário no localStorage

--- AVA-CG/src/routes/Login/Login.jsx
+++ AVA-CG/src/routes/Login/Login.jsx
@@ -1,6 +1,7 @@
 //import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import './Login.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -9,41 +10,78 @@ const Login = () => {
 
     const handleLoginClick = (e) => {
         e.preventDefault();
-        // Verificação de email e senha padrão
-        if (email === "teste@gmail.com" && senha === "teste100") {
-            // Se as credenciais estiverem corretas, redirecione para a página de disciplinas
-            navigate("/disciplinas");
+
+        const userDataJSON = localStorage.getItem(email);
+        if (userDataJSON) {
+            const userData = JSON.parse(userDataJSON);
+            // Verificar se a senha inserida corresponde à senha armazenada
+            if (senha === userData.senha) {
+                // Se as credenciais estiverem corretas, redirecione para a página desejada
+                navigate("/disciplinas");
+            } else {
+                alert("Senha incorreta. Por favor, tente novamente.");
+            }
         } else {
-            // Caso contrário, exiba uma mensagem de erro ou realize outra ação desejada
-            alert("Credenciais inválidas. Por favor, tente novamente.");
+            alert("Usuário não cadastrado. Por favor, verifique seu email ou cadastre-se.");
         }
+
+        // Verificação de email e senha padrão
+        // if (email === "teste@gmail.com" && senha === "teste100") {
+        //     // Se as credenciais estiverem corretas, redirecione para a página de disciplinas
+        //     navigate("/disciplinas");
+        // } else {
+        //     // Caso contrário, exiba uma mensagem de erro ou realize outra ação desejada
+        //     alert("Credenciais inválidas. Por favor, tente novamente.");
+        // }
     }
 
     return (
         <div className="Area-Login">
-            <div className="Login">
-                <h1>Página de Login</h1>
-            </div>
-
-            <form className="login-Form">
-                <label htmlFor='email' className='loginLabel'>Email do usuário</label>
-                <input type='text' id='email' className='loginInput' value={email} onChange={(e) => setEmail(e.target.value)}/>
-
-                <label htmlFor='senha' className='loginLabel'>Senha</label>
-                <input type='password' id='senha'  className='loginInput' value={senha} onChange={(e) => setSenha(e.target.value)}/>
-
-                <div className="login-btn">
-                    {/* Remova o Link e o button type="submit" */}
-                    <button onClick={(e) => handleLoginClick(e)}>Entrar</button>
+            <div className="area-esquerda">
+                <div className="Conteúdo1">
+                    <div className="imagem1">
+                        <img src="./_6f869732-9047-475a-a3c9-f307d6e4d058-removebg-preview_1-removebg-preview.png" />
+                    </div>
+                    <div className="Bem-Vindo">
+                        <h5>BEM-VINDO 
+                            <br/>DE VOLTA!</h5>
+                        <p>comece a estudar agora 
+                            <br/>mesmo</p>
+                    </div>
                 </div>
-            </form>
-            <div className="Area-Register-btn">
-                <Link to='/Register'>
-                    <button className="Register-btn">
-                        É a sua primeira vez? Cadastre-se agora mesmo!
-                    </button>
-                </Link>
+                
             </div>
+            <div className="area-direita">
+                <div className="Conteúdo2">
+                    <div className="Entrar">
+                        <h5>Entrar</h5>
+                    </div>
+                    <form className="Login-Form">
+                        <input type='text' id='email' className='loginInput' value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+
+                        <input type='password' id='senha'  className='loginInput' value={senha} placeholder="Senha" onChange={(e) => setSenha(e.target.value)}/>
+
+                        <div className="login-btn">
+                            {/* Remova o Link e o button type="submit" */}
+                            <button onClick={(e) => handleLoginClick(e)}><b>Entrar</b></button>
+                        </div>
+                    </form>
+
+                    <div className="Area-Register-btn">
+                        <p>Novo por aqui?
+                            <Link to='/Register'>
+                                <button>
+                                    Cadastre-se
+                                </button>
+                            </Link>
+                        </p>
+                    
+                    </div>
+                </div>
+                
+                
+            </div>
+            
         </div>
     );
 }
