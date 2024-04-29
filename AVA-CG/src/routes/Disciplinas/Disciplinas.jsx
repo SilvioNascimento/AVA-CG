10eea773c9184ed5382b70b04a2796e31eade4f7
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-04-28 21:20:03 -0300

    Adicionando a disciplina e seu estilo

--- AVA-CG/src/routes/Disciplinas/Disciplinas.jsx
+++ AVA-CG/src/routes/Disciplinas/Disciplinas.jsx
@@ -1,19 +1,42 @@
 //import React from "react";
 
 import { Link } from "react-router-dom";
+import './Disciplinas.css';
 
 const Disciplinas = () => {
     return (
-        <div className="Area-btns-disciplina">
-            <Link to='/SistemasMultimidia'>
-                <button className="btn-Sistemas-Multimidia">
-                    <h1>Sistemas Multimídia</h1>
-                </button>
-            </Link>
+        <div className="Area-btns-disciplinas">
+                
+            <b>DISCIPLINAS</b>
+            <div className="btns-disciplinas">
 
-            <button className="btn-Disciplina-em-Operacao">
-                <h1>Disciplina 2</h1>
-            </button>
+                <Link to='/SistemasMultimidia'>
+                    <button className="btn-Sistemas-Multimidia">
+                        <h1>Sistemas Multimídia</h1>
+                    </button>
+                </Link>
+
+                <button className="btn-Matemática">
+                    <h1>Matemática</h1>
+                </button>
+
+                <button className="btn-Português">
+                    <h1>Português</h1>
+                </button>
+
+                <button className="btn-Física">
+                    <h1>Física</h1>
+                </button>
+
+                <button className="btn-Física">
+                    <h1>Física</h1>
+                </button>
+
+                <button className="btn-Física">
+                    <h1>Física</h1>
+                </button>
+            </div>
+            
         </div>
     );
 }
