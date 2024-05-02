970d36c1391c34d31d8710629d96ecc29fc998b5
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-05-02 02:55:54 -0300

    Estilizando a página de erro

--- AVA-CG/src/routes/Errors/ErrorPage.jsx
+++ AVA-CG/src/routes/Errors/ErrorPage.jsx
@@ -1,8 +1,18 @@
-//import React from "react";
+import './ErrorPage.css';
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <h1>Erro 404!</h1>
+        <div className="Area-Error">
+            <img src='./public/erro-aviso-para-pagina-web-banner-apresentacao-midia-social-documentos-post.png' />
+            <h1>Erro 404!</h1>
+
+            <Link to='/'>
+                <button className='btn-para-login'>
+                    Página de Login
+                </button>
+            </Link>
+        </div>
     );
 }
 
