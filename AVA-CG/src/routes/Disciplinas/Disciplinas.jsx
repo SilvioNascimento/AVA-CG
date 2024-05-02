e4bd29367ad167c1ca5673cdac4fec3c44debff9
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-05-02 02:54:55 -0300

    Alterando e estilizando as disciplinas

--- AVA-CG/src/routes/Disciplinas/Disciplinas.jsx
+++ AVA-CG/src/routes/Disciplinas/Disciplinas.jsx
@@ -1,42 +1,78 @@
-//import React from "react";
-
-import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
+import { Link, useNavigate } from "react-router-dom";
 import './Disciplinas.css';
 
 const Disciplinas = () => {
-    return (
-        <div className="Area-btns-disciplinas">
-                
-            <b>DISCIPLINAS</b>
-            <div className="btns-disciplinas">
 
-                <Link to='/SistemasMultimidia'>
-                    <button className="btn-Sistemas-Multimidia">
-                        <h1>Sistemas Multimídia</h1>
-                    </button>
-                </Link>
+    const navigate = useNavigate();
 
-                <button className="btn-Matemática">
-                    <h1>Matemática</h1>
-                </button>
-
-                <button className="btn-Português">
-                    <h1>Português</h1>
-                </button>
-
-                <button className="btn-Física">
-                    <h1>Física</h1>
-                </button>
-
-                <button className="btn-Física">
-                    <h1>Física</h1>
-                </button>
-
-                <button className="btn-Física">
-                    <h1>Física</h1>
-                </button>
-            </div>
+    const handleClick = () => {
+        Swal.fire({
+            title: 'Você tem certeza que quer realizar o Logout?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                navigate('/');  // Redireciona para a página principal
+            }
             
+        });
+    }
+
+    return (
+        <div>
+            <div className="Area-btns-disciplinas">
+                
+                <b>DISCIPLINAS</b>
+                <div className="btns-disciplinas">
+    
+                    <Link to='/SistemasMultimidia'>
+                        <button className="btn-Sistemas-Multimidia">
+                            <h1>Sistemas Multimídia</h1>
+                        </button>
+                    </Link>
+    
+                    <Link to='/Maintenance'>
+                        <button className="btn-Matemática">
+                            <h1>Matemática</h1>
+                        </button>
+    
+                    </Link>
+                    <Link to='/Maintenance'>
+                        <button className="btn-Português">
+                            <h1>Português</h1>
+                        </button>
+                    </Link>
+    
+                    <Link to='/Maintenance'>
+                        <button className="btn-Física">
+                            <h1>Física</h1>
+                        </button>
+                    </Link>
+    
+                    <Link to='/Maintenance'>                    
+                        <button className="btn-Física">
+                            <h1>Biologia</h1>
+                        </button>
+                    </Link>
+    
+                    <Link to='/Maintenance'>
+                        <button className="btn-Física">
+                            <h1>Artes</h1>
+                        </button>
+                    </Link>
+    
+    
+                    
+                </div>
+            </div>
+            <div className="Area-btns-config1">
+                <button onClick={() => handleClick()}>Sair</button>
+            </div>
         </div>
     );
 }
